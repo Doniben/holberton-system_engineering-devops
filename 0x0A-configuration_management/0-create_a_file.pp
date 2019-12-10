bd9e1ad7f5f984c6520002e852541920d2e7431b
Doniben <doniben@esperanto.co>
2019-12-10 03:41:40 +0000

    [FEATURE] Using Puppet, create a file in /tmp.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This is a configuration file of puppet that creates a file in /tmp
+
+file { 'holberton':
+ensure  => 'file',
+path    => '/tmp/holberton',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+}
