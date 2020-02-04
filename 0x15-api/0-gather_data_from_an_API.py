e107cda8544a650dcaf0b568db2d76e76276bf4e
Doniben <doniben@esperanto.co>
2020-02-04 04:25:43 +0000

    [UPDATE] test without try and except

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,32 +4,27 @@
 import requests
 import sys
 
-try:
+user = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+todos = 'https://jsonplaceholder.typicode.com/todos'
+request_td = requests.get(todos)
+request_usr = requests.get(user)
+imported_users = request_usr.json().get('name')
+imported_tasks = request_td.json()
 
-    user = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-    todos = 'https://jsonplaceholder.typicode.com/todos'
-    request_td = requests.get(todos)
-    request_usr = requests.get(user)
-    imported_users = request_usr.json().get('name')
-    imported_tasks = request_td.json()
+tasks = []
+completed_count = 0
+todos_count = 0
 
-    tasks = []
-    completed_count = 0
-    todos_count = 0
+for task in imported_tasks:
+    if task.get('userId') == int(sys.argv[1]):
+        todos_count += 1
+        if task.get('completed') is True:
+            completed_count += 1
+            tasks.append(task.get('title'))
 
-    for task in imported_tasks:
-        if task.get('userId') == int(sys.argv[1]):
-            todos_count += 1
-            if task.get('completed') is True:
-                completed_count += 1
-                tasks.append(task.get('title'))
+print ("Employee {} is done with tasks({}/{}):".format(imported_users,
+                                                       completed_count,
+                                                       todos_count))
 
-    print ("Employee {} is done with tasks({}/{}):".format(imported_users,
-                                                           completed_count,
-                                                           todos_count))
-
-    for task_lines in tasks:
-        print("     {}".format(task_lines))
-
-except Exception as errr:
-    pass
+for task_lines in tasks:
+    print("     {}".format(task_lines))
