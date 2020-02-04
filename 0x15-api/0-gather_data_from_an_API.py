eef7566a02d3eb9e6ed0f1d439a90f7385ecaa1a
Doniben <doniben@esperanto.co>
2020-02-04 04:20:53 +0000

    [UPDATE] adding try and except error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,27 +4,32 @@
 import requests
 import sys
 
-user = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-todos = 'https://jsonplaceholder.typicode.com/todos'
-request_td = requests.get(todos)
-request_usr = requests.get(user)
-imported_users = request_usr.json().get('name')
-imported_tasks = request_td.json()
+try:
 
-tasks = []
-completed_count = 0
-todos_count = 0
+    user = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    todos = 'https://jsonplaceholder.typicode.com/todos'
+    request_td = requests.get(todos)
+    request_usr = requests.get(user)
+    imported_users = request_usr.json().get('name')
+    imported_tasks = request_td.json()
 
-for task in imported_tasks:
-    if task.get('userId') == int(sys.argv[1]):
-        todos_count += 1
-        if task.get('completed') is True:
-            completed_count += 1
-            tasks.append(task.get('title'))
+    tasks = []
+    completed_count = 0
+    todos_count = 0
 
-print ("Employee {} is done with tasks({}/{}):".format(imported_users,
-                                                       completed_count,
-                                                       todos_count))
+    for task in imported_tasks:
+        if task.get('userId') == int(sys.argv[1]):
+            todos_count += 1
+            if task.get('completed') is True:
+                completed_count += 1
+                tasks.append(task.get('title'))
 
-for task_lines in tasks:
-    print("     {}".format(task_lines))
+    print ("Employee {} is done with tasks({}/{}):".format(imported_users,
+                                                           completed_count,
+                                                           todos_count))
+
+    for task_lines in tasks:
+        print("     {}".format(task_lines))
+
+except Exception as err:
+    pass
